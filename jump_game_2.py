d467b87b0f6073589ec13db7302679b25fdb2156
Eugene Mischenko <evgenjion@ya.ru>
2022-11-13 12:45:02 +0300

    Jump game 2 BFS solution

--- jump_game_2.py
+++ jump_game_2.py
@@ -5,7 +5,24 @@ from typing import List
 
 
 class Solution:
+    # BFS solution
     def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            l = r + 1
+            r = farthest
+            jumps += 1
+
+        return jumps
+
+    def jumpMySolutionWithNoHints(self, nums: List[int]) -> int:
         num_len = len(nums)
         current = 0
         jumps = 0
